791a48d7b1c410de0af74d128e2f881ee7df9d97
Aditya654103 <144650014+Aditya654103@users.noreply.github.com>
2023-10-14 23:34:37 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,8 @@ const Contact = () => {
   return (
     <div className="section">
       <h2>Contact Me</h2>
-      <p>Email: aviral.jain2021a@vitstudetn.ac.in</p>
-      <p>Phone: 9588217939</p>
-      <p>LinkedIn:https://www.linkedin.com/in/aviral-jain-098992247</p>
+      <p>Email: aditya.singh2021a@vitstudetn.ac.in</p>
+      <p>Phone: 7015756225</p>
     </div>
   );
 };
